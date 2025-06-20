14f350ad328dab99ec370a7729c6b0be41db84df
DarkSide0584 <hs.22u10712@btech.nitdgp.ac.in>
2025-06-20 17:24:08 +0530

    fix: use dynamic port for Render

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app=require('./src/app');
 
 
 
+const PORT = process.env.PORT || 3000;
+
 
 app.listen(3000,()=>{
-    console.log("Server is running on http://localhost:3000");
+    console.log(`Server is running on http://localhost:${PORT}`);
 })
